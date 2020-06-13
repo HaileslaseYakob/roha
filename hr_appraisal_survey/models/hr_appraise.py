8a996b17801ab9c7823e41193e9f34a4eefee358
Haile Yakob <haileyakob@gmail.com>
2020-06-13 04:30:43 +0300

    Add files via upload

--- hr_appraisal_survey/models/hr_appraise.py
+++ hr_appraisal_survey/models/hr_appraise.py
@@ -6,14 +6,20 @@ from odoo import api, fields, models
 class Appraise(models.Model):
     _inherit = "hr.appraisal"
     job_id = fields.Many2one('hr.job', string="Job", related="employee_id.job_id")
-    survey_id = fields.Many2one('survey.survey', related='job_id.appraisal_survey_id', string="Appraisal Survey", readonly=True)
+    probation_id = fields.Many2one('survey.survey', related='job_id.appraisal_survey_id', string="Probation Survey",
+                                readonly=True)
+    survey_id = fields.Many2one('survey.survey', related='job_id.probation_survey_id', string="Appraisal Survey", readonly=True)
     response_id = fields.Many2one('survey.user_input', "Response", ondelete="set null")
     quiz_score=fields.Float("Appraisal score", related='response_id.quizz_score')
+    probation_appraisal=fields.Boolean("Probation appraisal")
     def action_start_survey(self):
         self.ensure_one()
         # create a response and link it to this applicant
         if not self.response_id:
-            response = self.survey_id._create_answer(partner=self.env.user.partner_id)
+            if self.probation_appraisal:
+                response = self.probation_id._create_answer(partner=self.env.user.partner_id)
+            else:
+                response = self.survey_id._create_answer(partner=self.env.user.partner_id)
             self.response_id = response.id
         else:
             response = self.response_id
--- hr_appraisal_survey/models/hr_job.py
+++ hr_appraisal_survey/models/hr_job.py
@@ -11,5 +11,10 @@ class Job(models.Model):
         domain=[('category', '=', 'hr_appraisal')],
         help="Choose an Apparaisal form for this job position and you will be able to print/answer this to all applicants with this job position")
 
+    probation_survey_id = fields.Many2one(
+        'survey.survey', "Appraisal Form",
+        domain=[('category', '=', 'hr_appraisal')],
+        help="Choose an Apparaisal form for this job position and you will be able to print/answer this to all applicants with this job position")
+
     def action_print_survey(self):
         return self.appraisal_survey_id.action_print_survey()
