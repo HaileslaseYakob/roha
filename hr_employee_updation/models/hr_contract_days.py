70647d777f1c0b601f5eedb064b0108a6ba76901
Haile Yakob <haileyakob@gmail.com>
2020-06-14 13:08:50 -0700

    Update hr_contract_days.py

--- hr_employee_updation/models/hr_contract_days.py
+++ hr_employee_updation/models/hr_contract_days.py
@@ -32,5 +32,5 @@ class HrEmployeeContract(models.Model):
                                         string="Salary Structure Type")
     notice_days = fields.Integer(string="Notice Period", default=_get_default_notice_days)
 
-    wage = fields.Monetary('Wage', compute="_compute_wage",store=True,
+    wage = fields.Monetary('Wage', compute="_compute_wage",store=True,readonly=False,
                            help="Employee's monthly gross wage.")
